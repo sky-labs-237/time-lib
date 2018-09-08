46f370409fec62307de1626422f5cee13127bdec
BYM <yves.bimai@sky-labs.io>
2018-09-08 09:29:24 +0100

    3rd comment line added to hello_time.clj

--- src/hello_time.clj
+++ src/hello_time.clj
@@ -10,6 +10,7 @@
 ; test 
 ; another test
 ; add a 3rd comment line from github itself
+; add a 4th comment line
 
 (defn time-str
   "Returns a string representation of a datetime in the local time zone."
